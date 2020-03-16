e4d12b58f6b67a74ef8b475ae09186aaaf27c7c1
Matyáš Kopp <kopp@ufal.mff.cuni.cz>
2020-03-16 20:20:21 +0100

    fix distributing files beetween steps add notes

--- src/run_parczech.sh
+++ src/run_parczech.sh
@@ -11,8 +11,8 @@ export DATA_DIR=$PWD/out
 export SCRAPPER_CACHE=1 # For development ONLY!
 export SCRAPPER_FAST=1 # For development ONLY!
 export SHARED=.
-export TEITOK=.
-export TEITOK_CORPUS=corpus
+export TEITOK=./TEITOK
+export TEITOK_CORPUS=$TEITOK/projects/CORPUS
 
 set -o allexport
 if [ -f "config.sh" ]; then
@@ -26,6 +26,9 @@ function log {
   echo -e `date +"%Y-%m-%d %T"`"\t$1" >> ${SHARED}/parczech.log
 }
 
+function log_process {
+  echo "$pid steno_download" > ${SHARED}/current_process
+}
 
 
 log "STARTED $ID: $pid"
@@ -38,10 +41,24 @@ if [ -f 'current_process' ]; then
   exit 0;
 fi
 
-
+###############################
 ### Download stenoprotocols ###
-echo "$pid steno_download" > ${SHARED}/current_process
-log "downloading"
+#   input:
+#   output:
+#     new:
+#       downloader-yaml/$ID
+#         - contains exported yaml files (only for quick manual checkout)
+#       downloader-tei/$ID
+#         ./YYYY-SSS                     ## each session has its own directory
+#           - teifiles
+#         ./person.xml                   ## copied from previeous run
+#     update:
+#       downloader-tei/sha1sum.list      ## pairs shasum=/path/.../downloader-tei/$ID/YYYY-SSS/teifile.xml
+#
+###############################
+
+log_process "steno_download"
+log "downloading $ID"
 
 export CL_WORKDIR=$DATA_DIR/downloader
 export CL_OUTDIR_YAML=$DATA_DIR/downloader-yaml
@@ -58,7 +75,6 @@ if [ -f "$CL_OUTDIR_TEI/$LAST_ID/person.xml" ]; then
   echo "moving $CL_OUTDIR_TEI/$LAST_ID/person.xml"
   mkdir -p "$CL_OUTDIR_TEI/$ID"
   cp "$CL_OUTDIR_TEI/$LAST_ID/person.xml" "$CL_OUTDIR_TEI/$ID"
-  chmod +w "$CL_OUTDIR_TEI/$ID/person.xml"
 fi
 
 perl -I downloader/lib -I lib -I ${SHARED}/lib downloader/$CL_SCRIPT --tei $CL_OUTDIR_TEI --yaml $CL_OUTDIR_YAML --id $ID
@@ -70,7 +86,6 @@ touch $DOWNLOADER_TEI_HASHES
 
 for hf in `find "$CL_OUTDIR_TEI/$ID" -type f ! -name "person.xml" -exec sha1sum {} \;|tr -s ' '|tr ' ' '='`
 do
-	echo "hf=$hf"
   hash=${hf%=*}
   file=${hf##*/}
   filepath=${hf##*=}
@@ -82,12 +97,20 @@ do
   fi
 done
 
-# protect tei files
-find "$CL_OUTDIR_TEI/$ID" -type f -exec chmod -w {} \;
 
+###############################
+###    Download audio       ###
+#  input:
+#    downloader-tei/$ID        ## used for list of mp3 files (grepped !!!)
+#  output:
+#    audio-orig
+#      ./${ID}.audio.list      ## mp3 links that occure in new tei files
+#      www.psp.cz              ## directory structure with downloaded mp3 files
+###############################
 
-######################
-### Download audio ###
+log_process "audio_download"
+log "downloading audio $ID"
+
 export AUDIO_PATH_ORIG=$DATA_DIR/audio-orig
 mkdir -p $AUDIO_PATH_ORIG
 
@@ -95,9 +118,23 @@ grep -r "audio/mp3" $CL_OUTDIR_TEI/$ID|sed "s/.*url=\"//;s/\".*//" > $AUDIO_PATH
 wget --no-clobber --directory-prefix $AUDIO_PATH_ORIG --force-directories -w 1 -i $AUDIO_PATH_ORIG/${ID}.audio.list 2>&1 | grep -B 2 ' 404 ' > $AUDIO_PATH_ORIG/${ID}.404.list
 mv $AUDIO_PATH_ORIG/${ID}.audio.list $AUDIO_PATH_ORIG/${ID}.audio.list.done
 
+
+
+########################################
 ### Merge audio and enrich tei files ###
-export AUDIO_PATH_MERGED=$DATA_DIR/audio-merged
-export AUDIO_PATH_TEI=$DATA_DIR/audio-tei
+#  input:
+#    downloader-tei/$ID
+#    audio-orig/www.psp.cz
+#  output:
+#    audio-merged/$ID
+#    audio-tei/$ID
+###############################
+
+log_process "audio_merging"
+log "merging audio $ID"
+
+export AUDIO_PATH_MERGED=$DATA_DIR/audio-merged/${ID}
+export AUDIO_PATH_TEI=$DATA_DIR/audio-tei/${ID}
 mkdir -p $AUDIO_PATH_MERGED
 mkdir -p $AUDIO_PATH_TEI
 
@@ -108,7 +145,7 @@ do
   MERGED_AUDIO_TEI=$AUDIO_PATH_TEI/${tei#*tei/*/}
   AUDIO_LIST=`perl -I lib -MTEI::ParlaClarin::TEI -e 'my $tei=TEI::ParlaClarin::TEI->load_tei(file => $ARGV[0]);print join("\n",@{$tei->getAudioUrls()});$tei->addAudioFile($ARGV[1]); $tei->toFile(outputfile => $ARGV[2])' $tei $MERGED_AUDIO_FILE $MERGED_AUDIO_TEI`
 
-  echo $MERGED_AUDIO_FILE;
+  log "merging to $MERGED_AUDIO_FILE";
   mkdir -p ${MERGED_AUDIO_FILE%/*}
   mkdir $AUDIO_PATH_MERGED/tmp
   for url in $AUDIO_LIST
@@ -122,22 +159,73 @@ do
   ffmpeg -i "concat:"$(cat $AUDIO_PATH_MERGED/tmp/filelist | tr "\n" "|" | sed "s/|$//") -acodec copy $MERGED_AUDIO_FILE
   chmod -w $MERGED_AUDIO_FILE
   rm -rf $AUDIO_PATH_MERGED/tmp
-  echo "MERGED AUDIO: $MERGED_AUDIO_FILE"
 done
 
-### Anotate tei ###
-# anotate tei file in AUDIO_PATH_TEI -> prevents multiple anotations
-find $AUDIO_PATH_TEI -type f -name '*.xml' -exec $TEITOK/common/Scripts/xmltokenize.pl  {} \;
 
-# COPY anotated files
-for f in `find $AUDIO_PATH_TEI -type f -name '*.xml' -not -name "*.nt.xml" -exec realpath --relative-to $AUDIO_PATH_TEI {} \;`
-do
-  DIR=${f%/*}
-  mkdir -p $TEITOK_CORPUS/xmlfiles/$DIR
-  cp $AUDIO_PATH_TEI/$f $TEITOK_CORPUS/xmlfiles/$f
-done
 
-### remove overwriten tei from teitok ###
+
+###############################
+###     Tokenize tei        ###
+#  input:
+#    audio-tei/$ID
+#  output:
+#    tokenizer-tei/$ID
+###############################
+
+log_process "tei_tokenizing"
+log "tokenizing tei $ID"
+
+export TOKENIZER_TEI=$DATA_DIR/tokenizer-tei/${ID}
+mkdir -p $TOKENIZER_TEI
+
+rsync -a --prune-empty-dirs $AUDIO_PATH_TEI/ $TOKENIZER_TEI
+find $TOKENIZER_TEI -type f -name '*.xml' -exec perl $TEITOK/common/Scripts/xmltokenize.pl  {} \;
+
+
+###############################
+###     NameTag tei         ###
+#  input:
+#    tokenizer-tei/$ID
+#  output:
+#    nametag-tei/$ID
+###############################
+
+log_process "tei_nametagging"
+log "nametagging tei $ID"
+
+export NAMETAG_TEI=$DATA_DIR/nametag-tei/${ID}
+mkdir -p $NAMETAG_TEI
+
+# copy tokenized files (ignore backups *.nt.xml)
+rsync -a --prune-empty-dirs --exclude '*.nt.xml' $TOKENIZER_TEI/ $NAMETAG_TEI
+
+find $NAMETAG_TEI -type f -name '*.xml' > $NAMETAG_TEI/filelist
+perl NameTag-module/xmlnametag.pl --model $SHARED/NameTag-module/models/czech-cnec2.0-140304.ner --filelist $NAMETAG_TEI/filelist --debug
+
+
+###############################
+###     FINALIZE            ###
+#  input:
+#    nametag-tei/$ID
+###############################
+
+log_process "tei publishing"
+log "publishing tei $ID"
+
+export FINALIZE_INPUT=$NAMETAG_TEI
+export FINALIZE_EXCLUDE="*.nntg.xml"
+
+rsync -a --prune-empty-dirs --include "*.xml" --exclude "filelist" --exclude "$FINALIZE_EXCLUDE" $FINALIZE_INPUT/ $TEITOK_CORPUS/xmlfiles
+
+cp "$CL_OUTDIR_TEI/$ID/person.xml" "$TEITOK_CORPUS/Resources/person.xml"
+
+
+
+echo "TODO: link merged audio !!!"
+
+
+
+
 ### upload (new and updated) tei files to teitok ###
 
 
