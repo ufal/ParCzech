760444a7f5a2533483e499748a23ca19cd058cb9
Matyáš Kopp <kopp@ufal.mff.cuni.cz>
2020-06-29 17:05:12 +0200

    tokenizing <ref>

--- src/MorphoDiTa-module/xmlmorphodita.pl
+++ src/MorphoDiTa-module/xmlmorphodita.pl
@@ -2,6 +2,7 @@ use warnings;
 use strict;
 use open qw(:std :utf8);
 use Encode qw(decode encode);
+use XML::LibXML::PrettyPrint;
 use XML::LibXML;
 use Getopt::Long;
 use POSIX qw(strftime);
@@ -11,11 +12,12 @@ use Lingua::Interset::Converter;
 
 my $scriptname = $0;
 
-my ($debug, $test, $filename, $filelist, $mdita_model, $elements_names, $sentsplit, $mtagger, $no_backup_file);
+my ($debug, $test, $filename, $filelist, $mdita_model, $elements_names, $sentsplit, $mtagger, $no_backup_file, $sub_elements_names);
 
 my$xmlNS = 'http://www.w3.org/XML/1998/namespace';
 
-$elements_names = "u,seg,head";
+$elements_names = "seg,head";
+$sub_elements_names = "ref";
 my $word_element_name = 'w';
 my $punct_element_name = 'pc';
 my $sent_element_name = 's';
@@ -68,6 +70,7 @@ GetOptions ( ## Command line options
             'filelist=s' => \$filelist, # file that contains files (it increase speed of script - MorphoDiTa model is inicialized single time)
             'model=s' => \$mtagger, # morphodita tagger
             'elements=s' => \$elements_names,
+            'sub-elements=s' => \$sub_elements_names, # child elements that are also tokenized
             'sent=i' => \$sentsplit, # split into sentences
             'word-element=s' => \$word_element_name,
             'punct-element=s' => \$punct_element_name,
@@ -76,7 +79,7 @@ GetOptions ( ## Command line options
             );
 
 @tags = ('tag cs::pdt') unless @tags;
-
+my %sub_elements_names_filter = map {$_ => 1} split(',', $sub_elements_names);
 
 my %tag_converter = map {
                       my ($attr,$format) = split(' ',$_);
@@ -126,7 +129,6 @@ my $tokenizer = Ufal::MorphoDiTa::Tokenizer::newCzechTokenizer();
 my $forms = Ufal::MorphoDiTa::Forms->new();
 my $lemmas = Ufal::MorphoDiTa::TaggedLemmas->new();
 
-
 while($filename = shift @input_files) {
   $/ = undef;
   open FILE, $filename;
@@ -155,83 +157,135 @@ while($filename = shift @input_files) {
   }
   my @parents = $doc->findnodes('//text//*[contains(" '.join(' ',split(',',$elements_names)).' ", concat(" ",name()," "))]');
   while(my $parent = shift @parents) {
-    my @childnodes = $parent->childNodes(); # find all child tokens
-    $_->unbindNode() for @childnodes;
+    my $text = '';
+    my @childnodes = ();
+    for my $chnode ($parent->childNodes()) { # loop and unbind all child nodes
+      $chnode->unbindNode();
+      my $chtext='';
+      my $chtextsize=0;
+      if ( $chnode->nodeType == XML_TEXT_NODE || exists $sub_elements_names_filter{$chnode->nodeName}) {
+        $chtext = $chnode->textContent();
+        $chtextsize = length($chtext);
+      }
+print STDERR $chnode->nodeType,"\t\t",$chnode,"\n";
+      $text .= $chtext;
+      push @childnodes,{node => $chnode, text => $chtext, size => $chtextsize};
+    }
+print STDERR "TEXT:=========================\n", $text,"\n$parent\n=============================\n";
+
     #$parent->removeChildNodes();
 #    my @stack = ($parent);
 #    my $newxml = '';
+    my $sentNode = $parent;
+    my $space = '';
+    if($space) {
+      $parent->appendText($space);
+      undef $space;
+    }
 
-    my $space = undef;
-    while(my $chnode = shift @childnodes) {
-      my $sentNode = $parent;
-      if($space) {
-        $parent->appendText($space);
-        undef $space;
+    $tokenizer->setText($text);
+    my $ti = 0; # nodetext index
+    my $cti = 0; # current childnode text index
+    my $chi = 0; # childnode index
+    while($tokenizer->nextSentence($forms, undef)){ ### sentences loop
+      while (@childnodes < $chi
+           && $childnodes[$chi]->{size} == 0) { ### copy if no content should be tokenized
+        $parent->appendChild($childnodes[$chi]->{node});
+        $chi += 1;
+        $cti = 0;
       }
-      if ( $chnode->nodeType != XML_TEXT_NODE ) { # not text node
-        $parent->appendChild($chnode);
-      } else { # text node
-        my $text = $chnode->textContent();
-        $text =~ s/^(\s*)//;
-        $parent->appendText($1) if $1;
-        $tokenizer->setText($text);
-        my $ti = 0;
-        while($tokenizer->nextSentence($forms, undef)){
-          if($sentsplit){
-            $sentNode = XML::LibXML::Element->new( $sent_element_name );
-            $sentNode->setAttributeNS($xmlNS, 'id', "s-$sid");
-            if($space) {
-              $parent->appendText($space);
-              undef $space;
-            }
-            $parent->appendChild($sentNode);
-            $sid++;
-          }
-          $tagger->tag($forms, $lemmas);
 
-          for (my $i = 0; $i < $lemmas->size(); $i++) {
-            my $form = $forms->get($i);
-            my $lemma = $lemmas->get($i);
-            $ti += length($form);
-            my $tokenNode = XML::LibXML::Element->new( $lemma->{tag} =~ /^Z/ ? $punct_element_name : $word_element_name );
-            $tokenNode->setAttributeNS($xmlNS, 'id', "w-$wid");
-            $lemma->{lemma} =~ s/^(.+?)[-_`].*/$1/ unless $full_lemma;
-            $tokenNode->setAttribute('lemma', $lemma->{lemma});
-            for my $key (keys %tag_converter){
-              my ($attr, $format,$converter) = @{$tag_converter{$key}};
-              my $value = ! $converter ? $lemma->{tag} : $converter->convert($lemma->{tag});
-              $value = $known_tag_fixes{"$attr $format"}->($value) if exists $known_tag_fixes{"$attr $format"};
-
-              $tokenNode->setAttribute($attr, ($tokenNode->hasAttribute($attr) ? $tokenNode->getAttribute($attr).' ' : '').$value);
-            }
-            # $tokenNode->setAttribute('form', $form);
-            $tokenNode->appendText($form);
-            if($space){
-              $sentNode->appendText($space);
-              undef $space;
-            }
-
-            $sentNode->appendChild($tokenNode);
-
-            if(substr($text, $ti, 1) =~ m/\s/){ # skip first space and append space to node
-              $space = " ";
-              $ti++;
-            } else {
-              $tokenNode->setAttribute('join', 'right');
-            }
-            $ti++ while substr($text, $ti, 1) =~ m/\s/; # skip next spaces
-
-            $wid++;
-          }
+      if($sentsplit){
+        $sentNode = XML::LibXML::Element->new( $sent_element_name );
+        $sentNode->setAttributeNS($xmlNS, 'id', "s-$sid");
+        if($space) {
+          $parent->appendText($space);
+          undef $space;
         }
+        $parent->appendChild($sentNode);
+        $sid++;
       }
-    } # end of element
+      $tagger->tag($forms, $lemmas);
+
+      for (my $i = 0; $i < $lemmas->size(); $i++) { ### tokens loop
+        my $form = $forms->get($i);
+        my $lemma = $lemmas->get($i);
+        $ti += length($form);
+        while ( $chi < @childnodes
+             && $childnodes[$chi]->{size} == 0) { ### copy if no content should be tokenized
+          $sentNode->appendChild($childnodes[$chi]->{node}->cloneNode(1));
+          $chi += 1;
+          $cti = 0;
+        }
+        if( $childnodes[$chi]->{node}->nodeType != XML_TEXT_NODE
+            && $cti == 0) { ### is it first token?
+          my $newchild = $childnodes[$chi]->{node}->cloneNode(0); # Expecting only text child nodes - no deep copy
+          $sentNode->appendChild($newchild);
+          $sentNode = $newchild; ## TODO use stack ???
+        }
+
+        $cti += length($form);
+        my $tokenNode = XML::LibXML::Element->new( $lemma->{tag} =~ /^Z/ ? $punct_element_name : $word_element_name );
+        $tokenNode->setAttributeNS($xmlNS, 'id', "w-$wid");
+        $lemma->{lemma} =~ s/^(.+?)[-_`].*/$1/ unless $full_lemma;
+        $tokenNode->setAttribute('lemma', $lemma->{lemma});
+
+        for my $key (keys %tag_converter){
+          my ($attr, $format,$converter) = @{$tag_converter{$key}};
+          my $value = ! $converter ? $lemma->{tag} : $converter->convert($lemma->{tag});
+          $value = $known_tag_fixes{"$attr $format"}->($value) if exists $known_tag_fixes{"$attr $format"};
+
+          $tokenNode->setAttribute($attr, ($tokenNode->hasAttribute($attr) ? $tokenNode->getAttribute($attr).' ' : '').$value);
+        }
+        # $tokenNode->setAttribute('form', $form);
+        $tokenNode->appendText($form);
+        if($space){
+          $sentNode->appendText($space);
+          undef $space;
+        }
+
+        $sentNode->appendChild($tokenNode);
+
+        if(substr($text, $ti, 1) =~ m/\s/){ # skip first space and append space to node
+          $space = " ";
+          $ti++;
+          $cti++; #
+        } else {
+          $tokenNode->setAttribute('join', 'right');
+        }
+        $ti++ while substr($text, $ti, 1) =~ m/\s/; # skip next spaces ???? this should not happen !!!
+
+        $wid++;
+        if($childnodes[$chi]->{size} <= $cti){
+          $sentNode = $sentNode->parentNode() if $childnodes[$chi]->{node}->nodeType != XML_TEXT_NODE;
+          $chi++;
+          $cti = 0;
+        }
+      } # end tokens loop
+    } # end sentences loop
+    while ( $chi < @childnodes
+           && $childnodes[$chi]->{size} == 0) { ### copy if no content should be tokenized
+      $sentNode->appendChild($childnodes[$chi]->{node}->cloneNode(1));
+      $chi += 1;
+      $cti = 0;
+    }
+
   } # end of file
   # Add a revisionDesc to indicate the file was tagged with NameTag
   my $revnode = makenode($doc, "/TEI/teiHeader/revisionDesc/change[\@who=\"xmlmorphodita\"]");
   my $when = strftime "%Y-%m-%d", localtime;
   $revnode->setAttribute("when", $when);
   $revnode->appendText("tokenized, tagged and lemmatized using xmlmorphodita.pl");
+  my $pp = XML::LibXML::PrettyPrint->new(
+    indent_string => "  ",
+    element => {
+        inline   => [qw//], # note
+        #block    => [qw//],
+        #compact  => [qw//],
+        preserves_whitespace => [qw/s/],
+        }
+    );
+  $pp->pretty_print($doc);
 
   my $xmlfile = $doc->toString;
 
