63b7baa969c214efed5301cc66ac85903cb4dca6
Matyáš Kopp <kopp@ufal.mff.cuni.cz>
2021-02-01 09:56:07 +0100

    add wrapping script for xml links checking

---
+++ src/parlaMint/check-links.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+
+PWD=`pwd`
+D=`dirname $0`
+
+
+usage() {
+  echo -e "Usage: $0 -i INDIR " 1>&2
+  exit 1
+}
+
+while getopts  ':i:c:'  opt; do
+  case "$opt" in
+    'i')
+      INDIR=$OPTARG
+      ;;
+    'c')
+      CONFIG_FILE=$OPTARG
+      ;;
+    *)
+      usage
+  esac
+done
+
+set -o allexport
+if [ -f "$CONFIG_FILE" ]; then
+  echo "loading config $CONFIG_FILE"
+  source "$CONFIG_FILE"
+fi
+set +o allexport
+
+
+if [ -z $XSL_TRANSFORM ] ; then
+  echo "no transformer $XSL_TRANSFORM"
+  usage
+fi
+
+CORPUS=`realpath $INDIR/ParlaMint-CZ.xml`
+
+for FILE in $INDIR/ParlaMint-CZ_*.xml ;
+do
+  $XSL_TRANSFORM "$D/ParlaMint/Scripts/check-links.xsl" $FILE 'XXX' meta="$CORPUS" 2>&1| grep "ERROR" | sed "s@^@$FILE\n\t@"
+done
