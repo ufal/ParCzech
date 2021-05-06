6127a27e08ac8595292c112f865b9ad34b2bf6c2
Matyáš Kopp <kopp@ufal.mff.cuni.cz>
2021-05-06 16:18:23 +0200

    add certainity to timelines (#120)

--- src/audio/audio-timeline.pl
+++ src/audio/audio-timeline.pl
@@ -14,6 +14,17 @@ use ParCzech::PipeLine::FileManager "audio-timeline";
 my $scriptname = $0;
 my $dirname = dirname($scriptname);
 
+my %filename_template = (
+  words => 'words_%s.tsv',
+  stats => 'stats_%s.tsv',
+  );
+my $cert_column = 'normalized_dist_with_gaps_75';
+my %cert = ( # normalized character mismatch is lower than
+    high => 0.3,
+    medium => 0.75,
+    low => 1,
+  );
+my @cert_order = sort {$cert{$a} <=> $cert{$b}} keys %cert;
 
 my ($debug, $test, $sync_dir);
 my %variables = ();
@@ -47,7 +58,7 @@ while($current_file = ParCzech::PipeLine::FileManager::next_file('tei', xpc => $
       $ParCzech::PipeLine::FileManager::logger->log_line("Wrong or missing audio url $audio_id:",($audio->getAttribute('url')//''));
       next;
     }
-    my $words_file = File::Spec->catfile($sync_dir,"words_$audio_date.txt");
+    my $words_file = get_sync_file_path('words',$audio_date);
     unless (-r $words_file) {
       $ParCzech::PipeLine::FileManager::logger->log_line("Missing or not readable word file $audio_id: $words_file");
       next;
@@ -60,6 +71,8 @@ while($current_file = ParCzech::PipeLine::FileManager::next_file('tei', xpc => $
     $timeline->setAttribute('unit','ms');
     $timeline->setAttribute('origin',"#$origin_id");
     $timeline->setAttribute('corresp',"#$audio_id");
+    my $cert_val = get_cert($audio_date);
+    $timeline->setAttribute('cert',"$cert_val") if defined $cert_val;
     $body->appendChild($timeline);
     # origin
     my $origin = XML::LibXML::Element->new('when');
@@ -107,6 +120,29 @@ sub add_timeline_point {
   my $anchor = XML::LibXML::Element->new('anchor');
   $anchor->setAttribute('synch',"#$id");
   return $anchor;
+}
+
+sub get_sync_file_path {
+  my ($type,$date) = @_;
+  return File::Spec->catfile($sync_dir,sprintf($filename_template{$type},$date));
+}
+
+sub get_cert {
+  my $date = shift;
+  my $stats_file = File::Spec->catfile($sync_dir,sprintf($filename_template{stats},$date));
+  unless (-r $stats_file) {
+    $ParCzech::PipeLine::FileManager::logger->log_line("Missing or not readable word file $date: $stats_file");
+    return;
+  }
+  open my $statfh, "<:encoding(utf8)", "$stats_file" or return;
+  my @header = $tsv->header ($statfh); # read header
+  my $row = $tsv->getline_hr($statfh);
+  close $statfh;
+  if ($row){
+    for my $c (@cert_order){
+      return $c if $row->{$cert_column} <= $cert{$c};
+    }
+  }
 }
 
 sub usage_exit {
