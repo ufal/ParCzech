111126f74f3f4a9866f826dcc8f325cffcc01848
Matyáš Kopp <kopp@ufal.mff.cuni.cz>
2022-08-05 14:43:14 +0200

    add tool for patching wrong/incomplete records in psp database

--- src/psp-db/pspdb.sh
+++ src/psp-db/pspdb.sh
@@ -55,6 +55,7 @@ wget -O "$OUTPUT_DIRECTORY/poslanci.zip"  https://www.psp.cz/eknih/cdrom/opendat
 ## unpack
 unzip -o "$OUTPUT_DIRECTORY/poslanci.zip" -d "$OUTPUT_DIRECTORY/poslanci"
 
+sed -i -f <(sed 's#\s*=>\s*#\\s*$/#;s#$#/#;s#^#s/^#' $D/patch_db.map) $OUTPUT_DIRECTORY/poslanci/*.unl
 
 perl -I $D/../lib $D/pspdb-generate-xml.pl --merge-to-events --person-list "$PERSONLIST" "${PARAMS[@]}" --translations "$D/translations.unl" --patches "$D/patches.unl" --roles-patches "$D/roles-patches.unl" --output-dir "$OUTPUT_DIRECTORY" --input-db-dir "$OUTPUT_DIRECTORY"
 
